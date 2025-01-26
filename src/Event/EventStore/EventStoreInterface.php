ca9a06732c5e64a8b6bfa3ba18f2202a99ab9bec
Twan Haverkamp <twan.haverkamp@outlook.com>
2025-01-26 18:13:46 +0100

    Add missing @throw annotations

--- src/Event/EventStore/EventStoreInterface.php
+++ src/Event/EventStore/EventStoreInterface.php
@@ -3,11 +3,17 @@
 namespace TwanHaverkamp\EventSourcingWithPhp\Event\EventStore;
 
 use TwanHaverkamp\EventSourcingWithPhp\Aggregate\AggregateInterface;
+use TwanHaverkamp\EventSourcingWithPhp\Event\Exception;
 
 interface EventStoreInterface
 {
+    /**
+     * @throws Exception\EventStorageFailedException
+     */
     public function load(AggregateInterface $aggregate): void;
 
-
+    /**
+     * @throws Exception\EventStorageFailedException
+     */
     public function save(AggregateInterface $aggregate): void;
 }
